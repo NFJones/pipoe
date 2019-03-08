0b1b884db30faec4bb5311dbb147e545a0b99509
Neil Jones <neil.franklin.jones@gmail.com>
2019-03-07 18:49:29 -0800

    Bumped to v2019.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r") as infile:
 
 setup(
     name="genpybb",
-    version="2019.3",
+    version="2019.4",
     description="Generate python bitbake recipes from pypi metadata.",
     long_description=long_description,
     long_description_content_type="text/markdown",
